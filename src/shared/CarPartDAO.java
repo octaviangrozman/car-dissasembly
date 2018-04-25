149eda37b3abf6cbed94d1d8c70abeb72980f7c1
Alina <alina.kaly@gmail.com>
2018-04-25 09:18:34 +0200

    Updated CarPartDAO

--- src/shared/CarPartDAO.java
+++ src/shared/CarPartDAO.java
@@ -7,8 +7,8 @@ import java.util.Collection;
 public interface CarPartDAO extends Remote {
    CarPartDTO create(double weight, int chassisNo, String model, PartType type) throws RemoteException;
    Collection<CarPartDTO> readAll() throws RemoteException;
-   void update(CarPartDTO car) throws RemoteException;
-   void delete(CarPartDTO car) throws RemoteException;
+   void update(CarPartDTO carPart) throws RemoteException;
+   void delete(CarPartDTO carPart) throws RemoteException;
    CarPartDTO read(int id) throws RemoteException;
 }
 
